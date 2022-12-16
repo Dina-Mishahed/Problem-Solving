67b3b89fb19fc86d1708d60126f033368b55c02a
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2022-12-16 11:27:41 +0200

    Edit max profit

--- LeetCode/maxProfit.java
+++ LeetCode/maxProfit.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int l=prices.length;
-
         int maxProfit = 0;
         int minPrice = prices[0];
-        for(int i=1; i<l; i++) {
-            maxProfit = Math.max(maxProfit, prices[i]-minPrice);
-            minPrice = Math.min(minPrice, prices[i]);
+        for(int i=1; i<l; i++) 
+        {
+            if(prices[i]-minPrice > maxProfit)
+                maxProfit =prices[i]-minPrice;
+            if(minPrice > prices[i])
+                minPrice = prices[i];
         }
-
         return maxProfit;
-            }
+    }
 } 
