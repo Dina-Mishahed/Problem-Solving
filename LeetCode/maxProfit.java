26b2aafc6fcaca84e2385448798a52195fdc1428
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2022-12-16 11:20:39 +0200

    max proft solution

--- LeetCode/maxProfit.java
+++ LeetCode/maxProfit.java
@@ -1,6 +1,14 @@
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        
-    }
-};
+    public int maxProfit(int[] prices) {
+        int l=prices.length;
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for(int i=1; i<l; i++) {
+            maxProfit = Math.max(maxProfit, prices[i]-minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+            }
+} 
